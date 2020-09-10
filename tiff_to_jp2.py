c13622bf4ed1ae44e9ca1ac1719763a89bf5edbd
Patrick <pgalligan@rockarch.org>
2020-09-10 13:54:53 -0400

    update derivative creation script
    use argparse for derivative creation variables.

--- tiff_to_jp2.py
+++ tiff_to_jp2.py
@@ -1,14 +1,33 @@
+import argparse
 import os
 
 from pathlib import Path
 
+PROGRESSION_ORDER_CHOICES = ["LRCP","RLCP","RPCL","PCRL","CPRL""]
 
-input = "/Users/pgalligan/Desktop/image_compression/masters/"
-tmp = "/Users/pgalligan/Desktop/image_compression/tmp/"
-output = "/Users/pgalligan/Desktop/image_compression/derivatives/"
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_directory", help="The full directory path of the original image files to create derivatives from (ex. /Documents/originals/)")
+    parser.add_argument("output_directory", help="The full directory path to store derivative files in (ex. /Documents/derivatives/)")
+    parser.add_argument("compression_ratio", default=1, help="Compression ratio values (ex. 1.5, 2, 2.2)")
+    parser.add_argument("resolution_number", default=6, help="Number of resolutions in whole numbers (ex. 4, 6, 7)")
+    parser.add_argument("precinct_size", help="Precinct size. Values specified must be power of 2. (ex. [256,256]) or [256,256],[128,128], etc.")
+    parser.add_argument("block_size", default=64,64, help="Code-block size. Maximum and default is 64x64. Nothing smaller than 4. (ex. 64,64 or 32,32)")
+    parser.add_argument("progression_order", choices=PROGRESSION_ORDER_CHOICES, default="RPCL", help="Progression order. The five progression orders are : LRCP, RLCP, RPCL, PCRL and CPRL")
+    return parser
 
-for file in os.listdir(input):
-    input_file = input + file
-    fname = file.split(".")[0]
-    output_file = output + fname + ".jp2"
-    os.system("opj_compress -i " + input_file + " -o " + output_file + " -r 1.5 -n 7 -c '[256,256],[256,256],[128,128]' -b '64,64' -p RPCL -SOP")
+def main():
+     """Main function, which is run when this script is executed"""
+    start_time = time.time()
+    parser = get_parser()
+    args = parser.parse_args()
+    for file in os.listdir(args.input_directory):
+        input_file = "{}{}".format(args.input_directory, file)
+        fname = file.split(".")[0]
+        output_file = "{}{}.jp2".format(args.output_directory, fname)
+        os.system("opj_compress -i {} -o {} -r {} -n {} -c '{}' -b '{}' -p {} -SOP".format(
+            input_file, output_file, args.compression_ratio, args.resolution_number, args.precinct_size,
+            args.block_size, args.progression_order))
+
+if __name__ == "__main__":
+    main()

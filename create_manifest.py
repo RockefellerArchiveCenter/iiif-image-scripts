ee4009b2a13241915f363def1836b159f07c8121
Patrick <pgalligan@rockarch.org>
2020-10-01 11:47:34 -0400

    add docstrings
    Adds docstrings to code.

--- create_manifest.py
+++ create_manifest.py
@@ -12,12 +12,19 @@ config = ConfigParser()
 config.read("local_settings.cfg")
 
 def get_parser():
+    """Defines and gets parser arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument("input_dir", help="The full directory path of the original image files to create derivatives from (ex. /Documents/originals/)")
     parser.add_argument("output_dir", help="The full directory path to store derivative files in (ex. /Documents/derivatives/)")
     return parser
 
 def clean_directories(input_dir, output_dir):
+    """Structures input and manifest directories based on whether they end in '/'.
+
+    Args:
+        input_dir (str): string representation of full path to directory with image files.
+        output_dir (str): string representation of full path to directory to save manifests.
+    """
     image_dir = args.input_dir if args.input_dir.endswith('/') else args.input_dir + '/'
     manifest_dir = args.output_dir if args.output_dir.endswith('/') else args.output_dir + '/'
     return image_dir, manifest_dir
@@ -27,6 +34,7 @@ args = parser.parse_args()
 fac = ManifestFactory()
 fac.set_debug("error")
 
+"""Sets directories for use in the manifest creation. Will need to change for dev/production."""
 image_dir, manifest_dir = clean_directories(args.input_dir, args.output_dir)
 fac.set_base_image_dir(image_dir)
 fac.set_base_prezi_dir(manifest_dir)
@@ -34,6 +42,7 @@ fac.set_base_prezi_uri("http://example.org/iiif/prezi/")
 fac.set_base_image_uri("http://example.org/iiif/image/")
 
 def authorize_as():
+    """Authorize an ArchivesSpace session."""
     global aspace
     aspace = ASpace(
         baseurl=config.get("ArchivesSpace", "baseurl"),
@@ -42,24 +51,41 @@ def authorize_as():
     )
 
 def get_ao(refid):
+    """Gets a JSON representation of an archival object.
+
+    Args:
+        refid (str): an ArchivesSpace refid
+    Returns:
+        ao (dict): a JSON representation of an archival object
+    """
     refs = aspace.client.get('repositories/2/find_by_id/archival_objects?ref_id[]={}'.format(refid)).json()
     ao_id = refs.get("archival_objects")[0].get("ref")
     ao = aspace.client.get(ao_id).json()
     return ao
 
 def get_identifiers(image_dir):
+    """Get a list of unique identifiers from files in a directory.
+
+    Args:
+        image_dir (str): a string representation of the directory containing image files.
+    Returns:
+        identifiers (lst): a list of unique identifiers.
+    """
     identifiers = []
     for file in os.listdir(image_dir):
         identifier = file.split('_')[0]
         identifiers.append(identifier)
     return list(set(identifiers))
 
-def get_page(file):
-    name = file.split('.')[0]
-    page = name.split('_')[-1]
-    return page
-
 def get_matching_files(ident, image_dir):
+    """Get a list of files that start with a specific refid.
+
+    Args:
+        ident (str): a string representation of an ArchivesSpace refid.
+        image_dir (str): a string representation of the directory containing image files.
+    Returns:
+        files (lst): a list of files that matched the identifier.
+    """
     files = []
     for file in os.listdir(image_dir):
         if file.startswith(ident):
@@ -67,6 +93,14 @@ def get_matching_files(ident, image_dir):
     return files
 
 def get_dimensions(file):
+    """Get the pixel height and width of an image file.
+
+    Args:
+        file (str): an image filename
+    Returns:
+        image_width (int): pixel width of an image
+        image_height (int): pixel height of an image
+    """
     with Image.open(file) as img:
         image_width, image_height = img.size
         return image_width, image_height
@@ -74,6 +108,9 @@ def get_dimensions(file):
 authorize_as()
 identifiers = get_identifiers(image_dir)
 for ident in identifiers:
+    """Sets the overall manifest labels, instantiate the manifest, and then
+    creates a sequence in that manifest for a specific identifier.
+    """
     # TO DO: Change Manifest Label to AO Title or inherit closest title
     manifest_label = ident.title()
     manifest = fac.manifest(ident=ident, label=manifest_label)
@@ -81,6 +118,9 @@ for ident in identifiers:
     page_number = 0
     files = sorted(get_matching_files(ident, image_dir))
     for file in files:
+        """Gets a refid from a file, and then creates a canvas and annotations
+        based on image dimensions and base prezi directories.
+        """
         ref = file[:-4]
         page_number = page_number + 1
         canvas_label = str(page_number)
--- tiff_to_jp2.py
+++ tiff_to_jp2.py
@@ -16,12 +16,21 @@ default_options = [
                    ]
 
 def get_parser():
+    """Defines and gets parser arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument("input_directory", help="The full directory path of the original image files to create derivatives from (ex. /Documents/originals/)")
     parser.add_argument("output_directory", help="The full directory path to store derivative files in (ex. /Documents/derivatives/)")
     return parser
 
 def get_dimensions(file):
+    """Gets pixel dimensions of a file.
+
+    Args:
+        file (str): filename of an image file.
+    Returns:
+        image_width (int): pixel width of an image
+        image_height (int): pixel height of an image
+    """
     with Image.open(file) as img:
         for x in img.tag[256]:
             image_width = x
@@ -30,11 +39,26 @@ def get_dimensions(file):
         return image_width, image_height
 
 def calculate_layers(width, height):
+    """Calculates the number of layers based on pixel dimensions.
+
+    Args:
+        width (int): width of an image
+        height (int): height of an image
+    Returns
+        layers (int): number of layers to convert to
+    """
     pixdem = max(width, height)
     layers = math.ceil((math.log(pixdem) / math.log(2)) - ((math.log(96) / math.log(2)))) + 1
     return layers
 
 def is_tiff(file):
+    """Checks whether a file is a tiff.
+
+    Args:
+        file (str): string representation of a filename.
+    Returns
+        boolean: True if tiff file, false otherwise.
+    """
     type = mimetypes.MimeTypes().guess_type(file)[0]
     if type == "image/tiff":
         return True
@@ -42,6 +66,16 @@ def is_tiff(file):
         return False
 
 def make_filenames(start_directory, end_directory, file):
+    """Make derivative filenames based on original filenames.
+
+    Args:
+        start_directory (str): the start directory with the original files.
+        end_directory (str): the ending directory for derivative creation.
+        file (str): string representation of a filename.
+    Returns
+        original_file (str): concatenated string of original directory and file.
+        derivative_file (str): concatenated string of end directory and file.
+    """
     original_file = "{}/{}".format(start_directory, file)
     fname = file.split(".")[0]
     derivative_file = "{}/{}.jp2".format(end_directory, fname)

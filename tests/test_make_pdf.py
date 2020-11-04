a0f41fb9b05016e84c830c897bb4e16ffbcf5af0
Hillel Arnold <helrond@hotmail.com>
2020-11-04 16:01:28 -0500

    adds docstrings to pdf tests

--- tests/test_make_pdf.py
+++ tests/test_make_pdf.py
@@ -11,11 +11,11 @@ UUIDS = [random_string() for x in range(random.randint(1,3))]
 PAGE_COUNT = random.randint(1,5)
 
 def setup():
+    """Sets up derivative directory."""
     if os.path.isdir(DERIVATIVE_DIR):
         shutil.rmtree(DERIVATIVE_DIR)
     shutil.copytree(FIXTURE_FILEPATH, DERIVATIVE_DIR)
     for f in os.listdir(DERIVATIVE_DIR):
-        print(f)
         for uuid in UUIDS:
             for page in range(PAGE_COUNT):
                 shutil.copyfile(
@@ -24,8 +24,13 @@ def setup():
         os.remove(os.path.join(DERIVATIVE_DIR, f))
 
 def test_run():
+    """Ensure the run method produces the expected number of files."""
     initial_length = len(os.listdir(DERIVATIVE_DIR))
     PDFMaker().run(DERIVATIVE_DIR)
     assert len(os.listdir(DERIVATIVE_DIR)) == initial_length + len(UUIDS)
     for uuid in UUIDS:
         assert os.path.isfile(os.path.join(DERIVATIVE_DIR, "{}.pdf".format(uuid)))
+
+def teardown():
+    """Remove derivative directory."""
+    shutil.rmtree(DERIVATIVE_DIR)

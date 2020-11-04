0e303dcc6f257b7508aeaa7aa22ef9bc508f1058
Hillel Arnold <helrond@hotmail.com>
2020-11-04 17:45:23 -0500

    debugging aws test

--- tests/test_aws_upload.py
+++ tests/test_aws_upload.py
@@ -3,7 +3,7 @@ import shutil
 import random
 
 import botocore.session
-from botocore.stub import Stubber
+from botocore.stub import Stubber, ANY
 
 from helpers import copy_sample_files, random_string
 from iiif_pipeline.aws_upload import UploadFiles
@@ -28,8 +28,7 @@ def setup():
 def test_s3_check():
     s3 = botocore.session.get_session().create_client("s3")
     head_response = {}  # TODO: add a response for head_object
-    upload_response = {}  # TODO: add response for upload_object
-    expected_params = {'Bucket': ANY}
+    expected_params = {'Bucket': ANY}  # TODO: add expected_params
     with Stubber(s3) as stubber:
         stubber.add_response("head_object", head_response, expected_params)
         found = UploadFiles().s3_check(DERIVATIVE_DIR, MANIFEST_DIR)

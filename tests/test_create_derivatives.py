fa5c6a4fb432de17571f16ce2c2ee36528584247
Hillel Arnold <helrond@hotmail.com>
2020-11-04 15:26:25 -0500

    add docstrings

--- tests/test_create_derivatives.py
+++ tests/test_create_derivatives.py
@@ -10,9 +10,14 @@ FIXTURES_FILEPATH = os.path.join("fixtures", "tif")
 SOURCE_DIR = os.path.join("/", "source")
 DERIVATIVE_DIR = os.path.join("/", "derivatives")
 UUIDS = [random_string() for x in range(random.randint(1,3))]
-PAGE_COUNT = 4
+PAGE_COUNT = random.randint(1,5)
 
 def setup():
+    """Sets up source directory for tests.
+
+    Duplicates the sample file into randomly-sized groups for randomly-generated
+    UUIDs.
+    """
     for d in [SOURCE_DIR, DERIVATIVE_DIR]:
         if os.path.isdir(d):
             shutil.rmtree(d)
@@ -25,5 +30,11 @@ def setup():
     os.makedirs(DERIVATIVE_DIR)
 
 def test_run():
+    """Ensure the run method produces the expected number of files."""
     DerivativeMaker().run(SOURCE_DIR, DERIVATIVE_DIR, random.choice(UUIDS), None)
     assert len(os.listdir(DERIVATIVE_DIR)) == PAGE_COUNT
+
+def teardown():
+    """Removes test directories."""
+    for d in [SOURCE_DIR, DERIVATIVE_DIR]:
+        shutil.rmtree(d)

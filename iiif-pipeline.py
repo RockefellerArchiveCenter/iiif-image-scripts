bbe531a4e5b2249692791d6bd3edf8ea80cbf444
Hillel Arnold <helrond@hotmail.com>
2020-11-10 16:38:24 -0500

    update variable description

--- iiif-pipeline.py
+++ iiif-pipeline.py
@@ -9,7 +9,7 @@ from iiif_pipeline.pipeline import IIIFPipeline
 
 def main():
     parser = argparse.ArgumentParser(description="Generates JPEG2000 images from TIF files based on input and output directories")
-    parser.add_argument("source_directory", help="The full directory path of the original image files to create derivatives from (ex. /Documents/originals/)")
+    parser.add_argument("source_directory", help="A directory containing subdirectories - named using ref ids - for archival objects")
     parser.add_argument("--skip", help="Skips files ending in `_001` during derivative creation.")
     args = parser.parse_args()
     IIIFPipeline().run(args.source_directory, args.skip)

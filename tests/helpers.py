aa774c757fcca224e251a4029bea428eb747b8d2
Hillel Arnold <helrond@hotmail.com>
2020-11-11 16:58:44 -0500

    improve pipeline test

--- tests/helpers.py
+++ tests/helpers.py
@@ -10,27 +10,33 @@ archivesspace_vcr = vcr.VCR(
     serializer='json',
     cassette_library_dir='fixtures/cassettes',
     record_mode='once',
-    match_on=['path', 'method', 'query'],
+    match_on=['path', 'method'],
     filter_query_parameters=['username', 'password'],
     filter_headers=['Authorization', 'X-ArchivesSpace-Session'],
 )
 
 
-def copy_sample_files(dir, identifiers, page_count, suffix):
+def copy_sample_files(directory, identifiers, page_count, suffix, to_master=False):
     """Duplicates a sample file.
 
     Args:
+        directory (str): Initial path of directory containing images to copy.
         identifiers (list): Identifiers used in filenames.
         page_count (int): The number of files to generate for each identifier.
         suffix (str): The filename suffix (extension) to be used
     """
-    for f in os.listdir(dir):
+    for f in os.listdir(directory):
         for ident in identifiers:
             for page in range(page_count):
+                target = os.path.join(directory, "{}_{}.{}".format(ident, page, suffix))
+                if to_master:
+                    target = os.path.join(directory, ident, "master", "{}_{}.{}".format(ident, page, suffix))
+                    if not os.path.isdir(os.path.join(directory, ident, "master")):
+                        os.makedirs(os.path.join(directory, ident, "master"))
                 shutil.copyfile(
-                    os.path.join(dir, f),
-                    os.path.join(dir, "{}_{}.{}".format(ident, page, suffix)))
-        os.remove(os.path.join(dir, f))
+                    os.path.join(directory, f),
+                    os.path.join(target))
+        os.remove(os.path.join(directory, f))
 
 
 def get_config():
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,29 +1,33 @@
 import os
+import random
 import shutil
 from unittest.mock import patch
 
 from iiif_pipeline.pipeline import IIIFPipeline
-from helpers import archivesspace_vcr
+from helpers import archivesspace_vcr, copy_sample_files, random_string
 
 SOURCE_DIR = os.path.join("/", "source")
+FIXTURES_FILEPATH = os.path.join("fixtures", "tif")
+UUIDS = [random_string() for x in range(random.randint(1, 3))]
+PAGE_COUNT = random.randint(1, 5)
 
 def setup():
     if os.path.isdir(SOURCE_DIR):
         shutil.rmtree(SOURCE_DIR)
-    os.makedirs(SOURCE_DIR)
+    shutil.copytree(FIXTURES_FILEPATH, SOURCE_DIR)
+    copy_sample_files(SOURCE_DIR, UUIDS, PAGE_COUNT, "tif", to_master=True)
 
 
-@patch("iiif_pipeline.clients.ArchivesSpaceClient.get_object")
 @patch("iiif_pipeline.clients.AWSClient.upload_files")
-@patch("iiif_pipeline.derivatives.create_jp2")
-@patch("iiif_pipeline.derivatives.create_pdf")
-@patch("iiif_pipeline.manifests.ManifestMaker.create_manifest")
-def test_main(mock_as_client, mock_aws_client, mock_jp2, mock_pdf, mock_manifest):
+def test_main(mock_aws_client):
+    expected_derivatives = len(UUIDS) * PAGE_COUNT
     with archivesspace_vcr.use_cassette("get_ao.json"):
         IIIFPipeline().run(SOURCE_DIR, False)
         for subpath in ["images", "pdfs", "manifests"]:
             assert os.path.isdir(os.path.join(SOURCE_DIR, subpath))
-        IIIFPipeline().run(SOURCE_DIR, True)
+        assert len(os.listdir(os.path.join(SOURCE_DIR, "images"))) == expected_derivatives
+        for subpath in ["pdfs", "manifests"]:
+            assert len(os.listdir(os.path.join(SOURCE_DIR, subpath))) == len(UUIDS)
 
 
 def teardown():
